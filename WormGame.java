9ca8546239ce6f12f25e7a8af7e8e4564391cc8c
jscapuzzo <32114348+jscapuzzo@users.noreply.github.com>
2017-11-01 00:10:39 -0400

    Worm Game in Development
    Added a score counter and worm food. Any time the worm touches the food the score increases and a new food spawns randomly. The exponential movement bug from the last patch was fixed. Handling the worm touching the boundaries is still being worked on. A known bug right now is if the worm touches a arbitrary boundary it will no longer accept movement input. The fruit may also spawn out of view of the game window.

--- WormGame.java
+++ WormGame.java
@@ -5,37 +5,124 @@ import java.awt.event.KeyEvent;
 import java.util.Random;
 
 public class WormGame extends JPanel{
-    static int xWorm = 0;
-    static int yWorm = 0;
-    int prevXWorm = 0;
-    int prevYWorm = 0;
-    int xFood;
-    int yFood;
+    static int xWorm = 8;
+    static int yWorm = 8;
+    int xFood = 64;
+    int yFood = 64;
+    boolean foodConsumed = false;
+    int score = 0;
+    boolean canMove = true;
     
     public void moveWormRight(){
-        prevXWorm = xWorm;
-        xWorm++;
+    	boolean inBounds = checkInBounds(xWorm);
+    	boolean letMove = checkMove(xWorm);
+    	if(inBounds == true){
+    		if(letMove == true){
+        		xWorm += 8;
+        	}
+        	else{
+        		letMove = true;
+        	}
+    	}
+    	else{
+    		inBounds = true;
+    	}
     }
     
     public void moveWormLeft(){
-        prevXWorm = xWorm;
-        xWorm--;
+    	boolean inBounds = checkInBounds(xWorm);
+    	boolean letMove = checkMove(xWorm);
+    	if(inBounds == true){
+    		if(letMove == true){
+        		xWorm -= 8;
+        	}
+        	else{
+        		letMove = true;
+        	}
+    	}
+    	else{
+    		inBounds = true;
+    	}
     }
     
     public void moveWormUp(){
-        prevYWorm = yWorm;
-        yWorm--;
+    	boolean inBounds = checkInBounds(yWorm);
+    	boolean letMove = checkMove(yWorm);
+    	if(inBounds == true){
+    		if(letMove == true){
+        		yWorm -= 8;
+        	}
+        	else{
+        		letMove = true;
+        	}
+    	}
+    	else{
+    		inBounds = true;
+    	}
     }
     
     public void moveWormDown(){
-        prevYWorm = yWorm;
-        yWorm++;
+    	boolean inBounds = checkInBounds(yWorm);
+    	boolean letMove = checkMove(yWorm);
+    	if(inBounds == true){
+    		if(letMove == true){
+        		yWorm += 8;
+        	}
+        	else{
+        		letMove = true;
+        	}
+    	}
+    	else{
+    		inBounds = true;
+    	}
+    }
+    
+    public boolean checkInBounds(int position){
+    	int p = position;
+    	if(p >= 400){
+    		return false;
+    	}
+    	else if(p < 0){
+    		return false;
+    	}
+    	else{
+    		return true;
+    	}
+    }
+    
+    public boolean checkMove(int position){
+    	int p = position;
+    	if(p + 1 >= 400){
+    		return false;
+    	}
+    	else if(p - 1 < 0){
+    		return false;
+    	}
+    	else{
+    		return true;
+    	}
+    }
+    
+    public void checkFood(){
+    	if(xWorm == xFood && yWorm == yFood){
+    		score++;
+    		moveFood();
+    	}
+    	else{
+    		// Do nothing
+    	}
     }
     
     public void moveFood(){
         Random ran = new Random();
         xFood = ran.nextInt(512);
         yFood = ran.nextInt(512);
+        while(xFood % 8 != 0){
+        	xFood = ran.nextInt(512);
+        }
+        while(yFood % 8 != 0){
+        	yFood = ran.nextInt(512);
+        }
     }
     
     public void paint(Graphics g){
@@ -44,20 +131,20 @@ public class WormGame extends JPanel{
         G.setColor(Color.WHITE);
         G.fillRect(0, 0, 512, 512);
         G.setColor(Color.BLACK);
-        G.fillRect(xWorm, yWorm, 10, 10);
-        //G.clearRect(prevXWorm, prevYWorm, 10, 10);
+        G.fillRect(xWorm, yWorm, 8, 8);
+        G.setColor(Color.GREEN);
+        G.fillRect(xFood, yFood, 8, 8);
+        G.setColor(Color.BLUE);
         G.drawString("Use the arrow keys to move!", 156, 156);
-        G.drawString("Collect the food!", 184, 166);
+        G.drawString("Score: " + String.valueOf(score), 184, 166);
         G.dispose();
     }
 
-    
     public static void main(String[] args) throws InterruptedException{
         JFrame f = new JFrame("Worm Game");
         final WormGame w = new WormGame();
-        f.setSize(512, 512);
+        f.setSize(528, 551); //These numbers allow the game itself to be 512 x 512
         f.add(w);
-        //f.setBackground(Color.WHITE);
         f.setVisible(true);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
@@ -71,35 +158,36 @@ public class WormGame extends JPanel{
         		}
         	}
         });
-        
+        System.out.println(f.getContentPane().getSize());
         while(true){
         	f.addKeyListener(new KeyAdapter(){
-        		@Override
-                public void keyTyped(KeyEvent e) {}
-
-                @Override
-                public void keyReleased(KeyEvent e) {}
-
                 @Override
                 public void keyPressed(KeyEvent e) {
-                	if(e.getKeyCode() == 37){
-                		w.moveWormLeft();
+                	if(w.canMove == true){
+	                	if(e.getKeyCode() == 37){
+	                		w.moveWormLeft();
+	                	}
+	                	else if(e.getKeyCode() == 38){
+	                		w.moveWormUp();
+	                	}
+	                	else if(e.getKeyCode() == 39){
+	                		w.moveWormRight();
+	                	}
+	                	else if(e.getKeyCode() == 40){
+	                		w.moveWormDown();
+	                	}
+	                	else{}
+	                	w.canMove = false;
                 	}
-                	else if(e.getKeyCode() == 38){
-                		w.moveWormUp();
+                	else{
+                		// Do nothing
                 	}
-                	else if(e.getKeyCode() == 39){
-                		w.moveWormRight();
-                	}
-                	else if(e.getKeyCode() == 40){
-                		w.moveWormDown();
-                	}
-                	else{}
-                	
                 }
             });
+        	w.checkFood();
             w.repaint();
             Thread.sleep(200);
+            w.canMove = true;
         }
     }
 }
