7255c3118c01d4d64d53425667087a13adb23ab2
ryagking <32530127+ryagking@users.noreply.github.com>
2017-12-03 11:43:51 -0500

    Able to make random blocks
    Everything needs to be tested.

--- TetrisBlock.java
+++ TetrisBlock.java
@@ -8,18 +8,25 @@
 
 public class TetrisBlock 
 {
-    	
 	private int[][] coordinates;
 	private String blockType; 
 
     private TetrisBlock block; 
 
+    /**
+     * Parameterized constructor. Makes a tetris block from input.
+     * @param coordinates
+     * @param type
+     */
     public TetrisBlock(int[][] coordinates, String type)
     {
         this.coordinates = coordinates;
         blockType = type;
     }
     
+    /**
+     * Default constructor. Creates a null block.
+     */
     public TetrisBlock()
     {
         coordinates = new int[4][2];
@@ -31,6 +38,48 @@ public class TetrisBlock
         return blockType;
     }
 
+    public TetrisBlock createRandomBlock()
+    {
+        int x = (int) Math.random() * 7 + 1;
+        
+        if(x == 1)
+        {
+        	return createZBlock();
+        }
+        
+        if(x == 2)
+        {
+        	return createTBlock();
+        }
+        
+        if(x == 3)
+        {
+        	return createLBlock();
+        }
+        
+        if(x == 4)
+        {
+        	return createMirrorLBlock();
+        }
+        
+        if(x == 5)
+        {
+        	return createLineBlock();
+        }
+        
+        if(x == 6)
+        {
+        	return createSquareBlock();
+        }
+        
+        if(x == 7)
+        {
+        	return createSBlock();
+        }
+        
+        return null;
+    }
+    
     public static TetrisBlock rotateLeft(TetrisBlock t) 
     {
         if (t.equals(createSquareBlock())) 
@@ -39,7 +88,15 @@ public class TetrisBlock
         }
         else
         {
-        	//rotation not yet implemented
+        	int i = 0;
+        	
+        	while(i < 4) //each array index has to be changed
+        	{
+                t.setXCoordinates(i, i);
+                t.setYCoordinates(i, i);
+                i++;
+            }
+        	
         	return t;
         }
     }
@@ -52,9 +109,27 @@ public class TetrisBlock
         }
         else
         {
-        	//rotation not yet implemented
+        	int i = 0;
+        	
+        	while(i < 4) //each array index has to be changed
+        	{
+                t.setXCoordinates(i, i);
+                t.setYCoordinates(i, i);
+                i++;
+            }
+        	
         	return t;
         }
+    }
+    
+    public void setXCoordinates(int row, int var)
+    {
+    	coordinates[row][0] = var; 
+    }
+    
+    public void setYCoordinates(int row, int var)
+    {
+    	coordinates[row][1] = var;
     }
     
     private static TetrisBlock createNullBlock()
