9c802100e1637cdfdf00b4a813c707216d9069e5
jscapuzzo <32114348+jscapuzzo@users.noreply.github.com>
2017-12-06 14:58:49 -0500

    Cleaned up the code. Still WIP.
    Removed unnecessary code. Removed the move methods. The game client will handle movement based on position data from this class.

--- SpaceObject.java
+++ SpaceObject.java
@@ -3,65 +3,33 @@ public class SpaceObject {
 	private int allyType;
 	// Value indicating what type of object it is.  0 = Barrier, 1 = Ship, 2 = Bullet
 	private int objectType = 3;
-	private int size = 0;
-	private int xPos = 0;
-	private int yPos = 0;
+	public int size = 0;
+	public int xPos = 0;
+	public int yPos = 0;
 	private final int BARRIER_SIZE = 32;
 	private final int SHIP_SIZE = 32;
 	private final int BULLET_SIZE = 4;
-	private boolean sizeKnown = false;
 	
-	public void moveObject(){
-		actualMoveObject();
-	}
-	
-	private void actualMoveObject(){
+	public void setSize(int ObjectType) {
 		if(objectType == 0){
-			xPos += BARRIER_SIZE;
-			yPos += BARRIER_SIZE;
+			size = BARRIER_SIZE;
 		}
 		else if(objectType == 1){
-			xPos += SHIP_SIZE;
-			yPos += SHIP_SIZE;
+			size = SHIP_SIZE;
 		}
 		else if (objectType == 2){
-			xPos += BULLET_SIZE;
-			yPos += BULLET_SIZE;
+			size = BULLET_SIZE;
 		}
 		else{
 			throw new IndexOutOfBoundsException("Value must be 0, 1, or 2!");
 		}
 	}
+	public void setPosition(int x, int y){
+		xPos = x;
+		yPos = y;
+	}
 	
-	public int returnSize() {
-		if(objectType == 0){
-			return BARRIER_SIZE;
-		}
-		else if(objectType == 1){
-			return SHIP_SIZE;
-		}
-		else if (objectType == 2){
-			return BULLET_SIZE;
-		}
-		else{
-			throw new IndexOutOfBoundsException("Value must be 0, 1, or 2!");
-		}
-	}
-	private void setPosition(){
-		if(objectType == 0){
-			
-		}
-		else if(objectType == 1){
-			
-		}
-		else if (objectType == 2){
-			
-		}
-		else{
-			throw new IndexOutOfBoundsException("Value must be 0, 1, or 2!");
-		}
-	}
-	private void initialSetup(int submittedAllyType, int submittedObjectType) {
+	private void initialSetup(int submittedAllyType, int submittedObjectType, int x, int y) {
 		if(submittedAllyType >= 0 && submittedAllyType <= 2){
 			allyType = submittedAllyType;
 			objectType = submittedObjectType;
@@ -75,10 +43,11 @@ public class SpaceObject {
 		else{
 			throw new IndexOutOfBoundsException("Value must be 0, 1, or 2!");
 		}
-		setPosition();
+		setPosition(x, y);
+		setSize(submittedObjectType);
 	}
 	
-	public SpaceObject(int submittedAllyType, int submittedObjectType) {
-		initialSetup(submittedAllyType, submittedObjectType);
+	public SpaceObject(int submittedAllyType, int submittedObjectType, int x, int y) {
+		initialSetup(submittedAllyType, submittedObjectType, x, y);
 	}
 }
